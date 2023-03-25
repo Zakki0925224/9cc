56a7b91dde3e20eae076d2d93d8ed70093880216
Zakki <zakki0925224@gmail.com>
2023-03-26 03:51:58 +0900

    [Add]: Added unary

--- 9cc.c
+++ 9cc.c
@@ -174,6 +174,7 @@ Node *new_num(int val)
 Node *expr();
 Node *mul();
 Node *primary();
+Node *unary();
 
 Node *expr()
 {
@@ -194,19 +195,30 @@ Node *expr()
 
 Node *mul()
 {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;)
     {
         if (consume('*'))
-            node = new_binary(ND_MUL, node, primary());
+            node = new_binary(ND_MUL, node, unary());
 
         else if (consume('/'))
-            node = new_binary(ND_DIV, node, primary());
+            node = new_binary(ND_DIV, node, unary());
 
         else
             return node;
     }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+        return unary();
+
+    if (consume('-'))
+        return new_binary(ND_SUB, new_num(0), unary());
+
+    return primary();
 }
 
 Node *primary()
